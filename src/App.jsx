a4554d84c41f395a46018a621e693ceb90e3e5c1
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-08-23 15:10:01 +0530

    display draw messages, apply style for statusMessage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { calculateWinner } from './components/winner';
 import Board from './components/Board';
 import { useState } from 'react';
 import './style.scss'
+import StatusMessage from './components/StatusMessage';
 
 function App() {
   const [squares,setSquares]= useState(Array(9).fill(null));
   const [isXnext, setisXnext]=useState(false);
 
-  const nextplayer= isXnext ? "X":"O";
   const winner =calculateWinner(squares);
-  const statusMessage =winner? `Winner Is ${winner}`: `Next Player Is ${nextplayer}`;
     
   const handleSquareClick = clickedPosition =>{
       if(squares[clickedPosition] || winner){
@@ -28,7 +27,7 @@ function App() {
 
   return ( 
     <div className='app'>
-      <h2>{statusMessage}</h2>
+      <StatusMessage winner={winner} isXnext={isXnext} squares={squares}/>
       <Board squares={squares} handleSquareClick={handleSquareClick}/>
     </div>
   );
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,20 @@
+const StatusMessage=({winner,isXnext,squares})=>{
+    const noMovesLeft=squares.every(squareValue=>squareValue!=null);
+
+    const nextplayer= isXnext ? "X":"O";
+    const renderStatusMessage=()=>{
+        if(winner){
+            return <div>Winner Is <span className={winner=="X"? "text-green": "text-orange" }>{winner}</span></div>
+        }
+        else if(!winner && noMovesLeft){
+            return <div>Game Is Draw</div>
+        }
+        else{
+            return <div>Next Player Is <span className={isXnext?"text-green":"text-orange"}>{nextplayer}</span></div>
+        }
+    };
+
+    return <div className="status-message">{renderStatusMessage()}</div>;
+};
+
+export default StatusMessage;
