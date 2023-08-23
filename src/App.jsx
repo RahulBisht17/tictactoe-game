d6884b1c3d41ba6eb83e1e83683c5af49d178126
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-08-23 13:47:44 +0530

    display X when clicking on square

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Board from './components/Board';
 import './style.scss'
 
 function App() {
-return ( 
-  <div className='app'>
-    <Board/> 
-  </div>
-);
+  return ( 
+    <div className='app'>
+      <Board/>
+    </div>
+  );
 }
 export default App
