b5b69c4a6338194302585412a747862286462c32
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-08-23 14:33:15 +0530

    lifted up state to app component, show status message, calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { calculateWinner } from './components/winner';
 import Board from './components/Board';
+import { useState } from 'react';
 import './style.scss'
 
 function App() {
+  const [squares,setSquares]= useState(Array(9).fill(null));
+  const [isXnext, setisXnext]=useState(false);
+
+  const nextplayer= isXnext ? "X":"O";
+  const winner =calculateWinner(squares);
+  const statusMessage =winner? `Winner Is ${winner}`: `Next Player Is ${nextplayer}`;
+    
+  const handleSquareClick = clickedPosition =>{
+      if(squares[clickedPosition] || winner){
+          return;
+      }
+      setSquares(currentSquares=>{
+          return currentSquares.map((squareValue, position)=>{
+              if (clickedPosition == position){
+                  return isXnext ? "X":"O";
+              }
+              return squareValue;
+          });
+      });
+      setisXnext((curr) => !curr);
+  };
+
   return ( 
     <div className='app'>
-      <Board/>
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick}/>
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
 import Square from "./Square"
 
-const Board=()=>{
-    const [squares,setSquares]= useState(Array(9).fill(null));
-    
-    const handleSquareClick = clickedPosition =>{
-        setSquares(currentSquares=>{
-            return currentSquares.map((squareValue, position)=>{
-                if (clickedPosition == position){
-                    return "X";
-                }
-                return squareValue;
-            });
-        });
-    };
-
+const Board=({squares, handleSquareClick})=>{
     const renderSquare=position=>{
         return (
             <Square value={squares[position]} onClick={()=> handleSquareClick(position)}/>
