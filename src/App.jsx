03424d917ebdbafc11b6af431b37b875d0b9aa1a
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-08-26 19:05:48 +0530

    moved new game state outside

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import './style.scss'
 import History from './components/History';
 import StatusMessage from './components/StatusMessage';
 
+const NEW_GAME=[{squares:Array(9).fill(null), isXnext:false}];
+
 function App() {
-  const [history,setHistory]=useState([{squares:Array(9).fill(null), isXnext:false}]);
+  const [history,setHistory]=useState(NEW_GAME);
   const [currentMove, setCurrentMove]=useState(0);
   const gamingBoard=history[currentMove];
 
